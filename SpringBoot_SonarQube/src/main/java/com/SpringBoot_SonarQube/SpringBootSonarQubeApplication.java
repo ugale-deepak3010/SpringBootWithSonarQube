47f714326eeabbb33b59987d22a6f6fae26a3396
Dipak Ugale <ugale.deepak3010@gmail.com>
2025-03-03 17:14:33 +0530

    empty created.

---
+++ SpringBoot_SonarQube/src/main/java/com/SpringBoot_SonarQube/SpringBootSonarQubeApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_SonarQube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSonarQubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSonarQubeApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_SonarQube/src/test/java/com/SpringBoot_SonarQube/SpringBootSonarQubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_SonarQube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSonarQubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
