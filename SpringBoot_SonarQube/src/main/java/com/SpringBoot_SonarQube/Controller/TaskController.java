4bcb4ceedead8a3f6677a1e3a5949368ad9e118c
Dipak Ugale <ugale.deepak3010@gmail.com>
2025-03-03 20:35:45 +0530

    Simple delete request has added!

--- SpringBoot_SonarQube/src/main/java/com/SpringBoot_SonarQube/Controller/TaskController.java
+++ SpringBoot_SonarQube/src/main/java/com/SpringBoot_SonarQube/Controller/TaskController.java
@@ -3,6 +3,7 @@ package com.SpringBoot_SonarQube.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,7 @@ import com.SpringBoot_SonarQube.Model.Task;
 import com.SpringBoot_SonarQube.Repository.TaskRepo;
 
 import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @RestController("task")
@@ -20,18 +21,26 @@ public class TaskController {
 
 	@Autowired
 	TaskRepo taskRepo;
-	
+
 	@GetMapping("/")
 	public List<Task> getTasks() {
 		return taskRepo.findAll();
 	}
-	
+
 	@PostMapping("/")
 	public Task postTask(@RequestBody Task task) {
 
-		//056710b0faaefe262014350182a13b7abdeba4e9 //SONAR_TOKEN
+		// 056710b0faaefe262014350182a13b7abdeba4e9 //SONAR_TOKEN
 		return taskRepo.save(task);
 	}
-	
-	
+
+	@DeleteMapping("/")
+	public String getMethodName(@RequestParam Long id) {
+
+		return taskRepo.findById(id).map(task -> {
+			taskRepo.delete(task);
+			return "Deleted Successfully!";
+		}).orElse("Task Not Found!");
+	}
+
 }
